bc041842246f7ab9caed549e89c978066c2b88d5
ANGEL MARCOS CASTILLA <ajmarcosc15@gmail.com>
2021-09-22 21:26:13 -0500

    corrigiendo el responsive

--- public/assets/js/showMenu.js
+++ public/assets/js/showMenu.js
@@ -1,13 +1,13 @@
-const showMenu = (idToggleMenu,idNavbar)=>{
+const showMenu = (idToggleMenu, idNavbar) => {
+
 const $menuIcon=document.getElementById(idToggleMenu);
 const $menu=document.getElementById(idNavbar);
-
-if($menuIcon && $menu){
-  $menuIcon.addEventListener("click",()=>{
-    $menu.classList.toggle("menu--active")
-    $menuIcon.classList.toggle("menu__icon--active")
-    
-  })
+  if($menuIcon && $menu){
+    $menuIcon.addEventListener("click",()=>{
+      $menu.classList.toggle("menu--active")
+      $menuIcon.classList.toggle("menu__icon--active")
+      
+    })
 }
 }
 
